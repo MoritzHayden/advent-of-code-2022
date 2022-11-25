1a7da5e9bf297839659d1becb01066d542005fe2
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-11-24 21:37:06 -0600

    Update output

--- src/main/java/com/haydenmoritz/aoc2022/days/Day01.java
+++ src/main/java/com/haydenmoritz/aoc2022/days/Day01.java
@@ -3,12 +3,13 @@ package com.haydenmoritz.aoc2022.days;
 import org.apache.commons.lang3.NotImplementedException;
 
 public class Day01 implements Day {
+    int dayNumber = 1;
 
     @Override
     public void solveAll() {
-        System.out.println("Solution (Day 1.1):");
+        System.out.println("Solution (Day " + dayNumber + ".1):");
         solvePart1();
-        System.out.println("Solution (Day 1.2):");
+        System.out.println("Solution (Day " + dayNumber + ".2):");
         solvePart2();
     }
 
