6195203fbe1f1179633c6925c7d808cb88cd3a50
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-11-25 11:43:27 -0600

    Remove unused import

--- src/main/java/com/haydenmoritz/aoc2022/Main.java
+++ src/main/java/com/haydenmoritz/aoc2022/Main.java
@@ -4,7 +4,6 @@ import com.haydenmoritz.aoc2022.days.*;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
