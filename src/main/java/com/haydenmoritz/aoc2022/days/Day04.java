fcb4737db72882f158bf54165b9d53929cf448f6
MoritzHayden <Moritz.Hayden@gmail.com>
2022-12-04 00:06:08 -0600

    Complete day 4

--- src/main/java/com/haydenmoritz/aoc2022/days/Day04.java
+++ src/main/java/com/haydenmoritz/aoc2022/days/Day04.java
@@ -1,8 +1,8 @@
 package com.haydenmoritz.aoc2022.days;
 
-import org.apache.commons.lang3.NotImplementedException;
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.haydenmoritz.aoc2022.utils.Utils.*;
 
@@ -17,10 +17,45 @@ public class Day04 implements IDay {
     }
 
     private String solvePart1() {
-        throw new NotImplementedException();
+        int containedPairs = 0;
+        for (String line : dayInput) {
+            String[] sectionAssignments = line.split(",");
+            List<Integer> rangeOne = Arrays.stream(sectionAssignments[0].split("-"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            List<Integer> rangeTwo = Arrays.stream(sectionAssignments[1].split("-"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            if (rangeOne.get(0) <= rangeTwo.get(0) && rangeOne.get(1) >= rangeTwo.get(1)) {
+                containedPairs++;
+            } else if (rangeTwo.get(0) <= rangeOne.get(0) && rangeTwo.get(1) >= rangeOne.get(1)) {
+                containedPairs++;
+            }
+        }
+
+        return String.valueOf(containedPairs);
     }
 
     private String solvePart2() {
-        throw new NotImplementedException();
+        int overlappingPairs = 0;
+        for (String line : dayInput) {
+            String[] sectionAssignments = line.split(",");
+            List<Integer> rangeOne = Arrays.stream(sectionAssignments[0].split("-"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            List<Integer> rangeTwo = Arrays.stream(sectionAssignments[1].split("-"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            for (int i = rangeOne.get(0); i <= rangeOne.get(1); i++) {
+                if (rangeTwo.get(0) <= i && i <= rangeTwo.get(1)) {
+                    overlappingPairs++;
+                    break;
+                }
+            }
+        }
+
+        return String.valueOf(overlappingPairs);
     }
 }
