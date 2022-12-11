2c0833137c476801dd233acb6f6e02e0cc85979e
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-12-11 15:15:13 -0600

    Complete day 10

--- src/main/java/com/haydenmoritz/aoc2022/days/Day10.java
+++ src/main/java/com/haydenmoritz/aoc2022/days/Day10.java
@@ -1,14 +1,16 @@
 package com.haydenmoritz.aoc2022.days;
 
-import org.apache.commons.lang3.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static com.haydenmoritz.aoc2022.utils.Utils.*;
 
 public class Day10 implements IDay {
     int dayNumber = 10;
     List<String> dayInput = readFile(dayNumber);
+    List<Integer> xRegisters = new ArrayList<>();
+    List<Integer> signalStrengths = new ArrayList<>();
 
     @Override
     public void solveAll() {
@@ -17,10 +19,80 @@ public class Day10 implements IDay {
     }
 
     private String solvePart1() {
-        throw new NotImplementedException();
+        int cycle = 0;
+        int xReg = 1;
+
+        for (String line : dayInput) {
+            Map<Instruction, Integer> instruction = parseInstruction(line);
+
+            if (instruction.containsKey(Instruction.NOOP)) {
+                // Cycle once
+                cycle++;
+                runCycle(xRegisters, signalStrengths, cycle, xReg);
+            } else if (instruction.containsKey(Instruction.ADDX)) {
+                // Cycle one
+                cycle++;
+                runCycle(xRegisters, signalStrengths, cycle, xReg);
+
+                // Cycle two
+                cycle++;
+                runCycle(xRegisters, signalStrengths, cycle, xReg);
+
+                // Perform operation
+                xReg += instruction.get(Instruction.ADDX);
+            }
+        }
+
+        return String.valueOf(sumNeededCycles(signalStrengths));
     }
 
     private String solvePart2() {
-        throw new NotImplementedException();
+        StringBuilder sb = new StringBuilder();
+        sb.append("BUCACBUZ (Image generated below ↓)\n");
+        for (int i = 0; i < 240; i++) {
+            int currentXReg = xRegisters.get(i);
+
+            if (i % 40 == currentXReg - 1 || i % 40 == currentXReg || i % 40 == currentXReg + 1) {
+                sb.append("#");
+            } else {
+                sb.append(".");
+            }
+
+            if (i == 39 || i == 79 || i == 119 || i == 159 || i == 199) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    private int calculateSignalStrength(int cycle, int xReg) {
+        return cycle * xReg;
+    }
+
+    private void runCycle(List<Integer> xRegisters, List<Integer> signalStrengths, int cycle, int xReg) {
+        xRegisters.add(xReg);
+        signalStrengths.add(calculateSignalStrength(cycle, xReg));
+    }
+
+    private int sumNeededCycles(List<Integer> signalStrengths) {
+        return signalStrengths.get(19) +
+                signalStrengths.get(59) +
+                signalStrengths.get(99) +
+                signalStrengths.get(139) +
+                signalStrengths.get(179) +
+                signalStrengths.get(219);
+    }
+
+    private Map<Instruction, Integer> parseInstruction(String line) {
+        String[] splitLine = line.split(" ");
+        if (splitLine[0].equals("noop")) return Map.of(Instruction.NOOP, 0);
+        if (splitLine[0].equals("addx")) return Map.of(Instruction.ADDX, Integer.parseInt(splitLine[1]));
+
+        throw new RuntimeException("Parsed invalid input");
+    }
+
+    enum Instruction {
+        NOOP,
+        ADDX
     }
 }
