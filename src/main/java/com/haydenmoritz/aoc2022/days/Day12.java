87566d2d6b6a48e86dee17358da60e2906eb9dd8
Hayden Moritz <Moritz.Hayden@gmail.com>
2022-12-12 21:33:06 -0600

    Add day 12 comments

--- src/main/java/com/haydenmoritz/aoc2022/days/Day12.java
+++ src/main/java/com/haydenmoritz/aoc2022/days/Day12.java
@@ -17,6 +17,13 @@ public class Day12 implements IDay {
     }
 
     private String solvePart1() {
+        // TODO:
+        //   1. Parse the input into a graph representation
+        //   2. Implement and run Dijkstra's algorithm on the graph
+        //   3. Use the ASCII decimal representation to get edge weight (cast char to int): https://www.asciitable.com/
+        //     - S = 83
+        //     - E = 69
+        //     - [a-z] = [97-122]
         throw new NotImplementedException();
     }
 
